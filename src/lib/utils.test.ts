d829b500a9ef74b3ae0ce93dda9b09682cf6ac1b
Max Gerber <max@stytch.com>
2025-07-08 15:59:00 -0700

    add test back in since test suite is empty

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,3 +1,8 @@
 import { describe, it, expect } from 'vitest'
 
 // All sanitizeUrl tests have been moved to the strict-url-sanitise package
+describe('sample test', () => {
+  it('Adds numbers', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
